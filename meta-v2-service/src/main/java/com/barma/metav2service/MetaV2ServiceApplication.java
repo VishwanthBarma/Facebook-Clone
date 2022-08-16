5de1f2d1012c549711d5c675df03342b9744784d
Barma Vishwanth <vishwanthbsavp@gmail.com>
2022-08-16 22:55:09 +0530

    initial commit

---
+++ meta-v2-service/src/main/java/com/barma/metav2service/MetaV2ServiceApplication.java
@@ -0,0 +1,13 @@
+package com.barma.metav2service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetaV2ServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetaV2ServiceApplication.class, args);
+	}
+
+}
---
+++ meta-v2-service/src/test/java/com/barma/metav2service/MetaV2ServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barma.metav2service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetaV2ServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
